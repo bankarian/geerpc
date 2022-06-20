91b04a5c454318603619a12802ef54477111de58
Beney Chen <beney-chen@qq.com>
2022-06-20 21:11:36 +0800

    gob codec

--- codec/codec.go
+++ codec/codec.go
@@ -1,7 +1,31 @@
 package codec
 
+import "io"
+
 type Header struct {
-	ServiceMethod string
+	ServiceMethod string // name format "Service.Method"
 	Seq           uint64 // sequence number chosen by client
 	Error         string
+}
+
+type Codec interface {
+	io.Closer
+	ReadHeader(*Header) error
+	ReadBody(interface{}) error
+	Write(*Header, interface{}) error
+}
+
+type NewCodecFunc func(io.ReadWriteCloser) Codec
+type Type string
+
+const (
+	GobType  Type = "application/gob"
+	JsonType Type = "application/json" // not implemented
+)
+
+var NewCodecFuncMap map[Type]NewCodecFunc
+
+func init() {
+	NewCodecFuncMap = make(map[Type]NewCodecFunc)
+	NewCodecFuncMap[GobType] = NewGobCodec
 }
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var u uint8 = 1
+	fmt.Println(^u)
+}
